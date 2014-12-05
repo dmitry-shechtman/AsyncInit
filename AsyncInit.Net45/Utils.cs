8d6f08a15d8cca70974b009c6247c7e9479a3a2f
damnian <damnian@gmail.com>
2014-12-06 01:47:47 +0200

    Added AsyncInit.Net45/Utils.cs

---
+++ AsyncInit.Net45/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ditto.AsyncInit
+{
+    /// <summary>
+    /// Provides utility methods for AsyncActivator.
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Creates an instance of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to create.</typeparam>
+        /// <returns>A reference to the newly created object.</returns>
+        public static T CreateInstance<T>()
+        {
+            return (T)Activator.CreateInstance(typeof(T), true);
+        }
+    }
+}
