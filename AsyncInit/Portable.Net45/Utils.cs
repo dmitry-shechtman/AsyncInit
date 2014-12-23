9be4c65aa3c76e31325952349d40a0bedc751f5a
Dmitry Shechtman <damnian@gmail.com>
2014-12-23 09:50:56 +0200

    Removed NetCore and Silverlight Added Portable.Net45

---
+++ AsyncInit/Portable.Net45/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Ditto.AsyncInit
+{
+    /// <summary>
+    /// Provides utility methods for AsyncActivator.
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Creates an instance of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to create.</typeparam>
+        /// <returns>A reference to the newly created object.</returns>
+        public static T CreateInstance<T>()
+        {
+            var typeInfo = typeof(T).GetTypeInfo();
+            var ctor = typeInfo.DeclaredConstructors.SingleOrDefault(c => c.GetParameters().Length == 0);
+            if (ctor == null)
+                throw new MissingMemberException("No parameterless constructor defined for this object.");
+            return (T)ctor.Invoke(null);
+        }
+    }
+}
