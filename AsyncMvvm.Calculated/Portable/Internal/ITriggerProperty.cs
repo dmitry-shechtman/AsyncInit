03f5ac89429d81976cf6afaca465f9c2d772609a
Dmitry Shechtman <damnian@gmail.com>
2014-12-28 10:56:03 +0200

    Moved AsyncMvvm to own repo

--- AsyncMvvm.Calculated/Portable/Internal/ITriggerProperty.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ditto.AsyncMvvm.Calculated.Internal
-{
-    internal interface ITriggerProperty<T> : IProperty<T>
-    {
-        void NotifyValueChanged(T value, string propertyName);
-    }
-}
