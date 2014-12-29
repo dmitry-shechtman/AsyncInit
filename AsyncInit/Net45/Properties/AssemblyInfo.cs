f04657af6d9865a320addc41a877aee915221ffa
Dmitry Shechtman <damnian@gmail.com>
2014-12-29 07:49:04 +0200

    Extracted CommonAssemblyInfo.cs and added Solution Items

--- AsyncInit/Net45/Properties/AssemblyInfo.cs
+++ AsyncInit/Net45/Properties/AssemblyInfo.cs
@@ -1,36 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Ditto.AsyncInit")]
-[assembly: AssemblyDescription(".NET 4.5 Ditto.AsyncInit library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dmitry Shechtman")]
-[assembly: AssemblyProduct("Ditto.AsyncInit")]
-[assembly: AssemblyCopyright("Copyright © Dmitry Shechtman 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e87ffb4-500a-4f2b-bdef-3735cf32741d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyDescription("A generic implementation of the asynchronous factory pattern.")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ComVisible(false)]
