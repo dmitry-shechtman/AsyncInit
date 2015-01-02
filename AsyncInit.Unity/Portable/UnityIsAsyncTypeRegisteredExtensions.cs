ffd05c3585b8102805acb2ad2efecf34c2b5359f
Dmitry Shechtman <damnian@gmail.com>
2015-01-02 19:19:13 +0200

    Added AsyncInit.Unity

---
+++ AsyncInit.Unity/Portable/UnityIsAsyncTypeRegisteredExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Ditto.AsyncInit.Services;
+using Microsoft.Practices.Unity;
+using System;
+
+namespace Ditto.AsyncInit.Unity
+{
+    /// <summary>
+    /// Adds a set of asynchronous registration check methods to <see cref="IUnityContainer"/>.
+    /// </summary>
+    public static class UnityIsAsyncTypeRegisteredExtensions
+    {
+        /// <summary>
+        /// Checks if a particular asynchronously initialized type has been registered with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to check.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <returns><value>true</value> if this type has been registered.</returns>
+        public static bool IsAsyncTypeRegistered<T>(this IUnityContainer container)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.IsRegistered<IAsyncInitializer<T>>();
+        }
+
+        /// <summary>
+        /// Checks if a particular asynchronously initialized type/name pair has been registered with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to check.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="name">The name to check.</param>
+        /// <returns><value>true</value> if this type/name pair has been registered.</returns>
+        public static bool IsAsyncTypeRegistered<T>(this IUnityContainer container, string name)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.IsRegistered<IAsyncInitializer<T>>(name);
+        }
+    }
+}
---
+++ AsyncInit.Unity/Portable/UnityRegisterAsyncTypeExtensions.cs
@@ -0,0 +1,267 @@
+﻿using Ditto.AsyncInit.Services;
+using Microsoft.Practices.Unity;
+using System;
+
+namespace Ditto.AsyncInit.Unity
+{
+    /// <summary>
+    /// Adds a set of asynchronous registration methods to <see cref="IUnityContainer"/>.
+    /// </summary>
+    public static class UnityRegisterAsyncTypeExtensions
+    {
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to register.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="injectionMembers">Injection configuration objects.</param>
+        public static IUnityContainer RegisterAsyncType<T>(this IUnityContainer container, params InjectionMember[] injectionMembers)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.RegisterType<IAsyncInitializer<T>, UnityAsyncInitializer<T, T>>(injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to register.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="lifetimeManager">The <see cref="LifetimeManager"/> that controls the lifetime of the returned instance.</param>
+        /// <param name="injectionMembers">Injection configuration objects.</param>
+        public static IUnityContainer RegisterAsyncType<T>(this IUnityContainer container, LifetimeManager lifetimeManager, params InjectionMember[] injectionMembers)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.RegisterType<IAsyncInitializer<T>, UnityAsyncInitializer<T, T>>(lifetimeManager, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to register.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="name">The name to use for registration.</param>
+        /// <param name="injectionMembers">Injection configuration objects.</param>
+        public static IUnityContainer RegisterAsyncType<T>(this IUnityContainer container, string name, params InjectionMember[] injectionMembers)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.RegisterType<IAsyncInitializer<T>, UnityAsyncInitializer<T, T>>(name, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to register.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="name">The name to use for registration.</param>
+        /// <param name="lifetimeManager">The <see cref="LifetimeManager"/> that controls the lifetime of the returned instance.</param>
+        /// <param name="injectionMembers">Injection configuration objects.</param>
+        public static IUnityContainer RegisterAsyncType<T>(this IUnityContainer container, string name, LifetimeManager lifetimeManager, params InjectionMember[] injectionMembers)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.RegisterType<IAsyncInitializer<T>, UnityAsyncInitializer<T, T>>(name, lifetimeManager, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to register.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="args">Initialization arguments.</param>
+        public static IUnityContainer RegisterAsyncType<T>(this IUnityContainer container, AsyncInitArgs args)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+			var injectionMembers = CreateInjectionMembers(args);
+            return container.RegisterType<IAsyncInitializer<T>, UnityAsyncInitializer<T, T>>(injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to register.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="lifetimeManager">The <see cref="LifetimeManager"/> that controls the lifetime of the returned instance.</param>
+        /// <param name="args">Initialization arguments.</param>
+        public static IUnityContainer RegisterAsyncType<T>(this IUnityContainer container, LifetimeManager lifetimeManager, AsyncInitArgs args)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+			var injectionMembers = CreateInjectionMembers(args);
+            return container.RegisterType<IAsyncInitializer<T>, UnityAsyncInitializer<T, T>>(lifetimeManager, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to register.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="name">The name to use for registration.</param>
+        /// <param name="args">Initialization arguments.</param>
+        public static IUnityContainer RegisterAsyncType<T>(this IUnityContainer container, string name, AsyncInitArgs args)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+			var injectionMembers = CreateInjectionMembers(args);
+            return container.RegisterType<IAsyncInitializer<T>, UnityAsyncInitializer<T, T>>(name, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="T">The type to register.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="name">The name to use for registration.</param>
+        /// <param name="lifetimeManager">The <see cref="LifetimeManager"/> that controls the lifetime of the returned instance.</param>
+        /// <param name="args">Initialization arguments.</param>
+        public static IUnityContainer RegisterAsyncType<T>(this IUnityContainer container, string name, LifetimeManager lifetimeManager, AsyncInitArgs args)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+			var injectionMembers = CreateInjectionMembers(args);
+            return container.RegisterType<IAsyncInitializer<T>, UnityAsyncInitializer<T, T>>(name, lifetimeManager, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="TFrom">The requested type.</typeparam>
+        /// <typeparam name="TTo">The actual type.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="injectionMembers">Injection configuration objects.</param>
+        public static IUnityContainer RegisterAsyncType<TFrom, TTo>(this IUnityContainer container, params InjectionMember[] injectionMembers)
+			where TTo : TFrom
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.RegisterType<IAsyncInitializer<TFrom>, UnityAsyncInitializer<TFrom, TTo>>(injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="TFrom">The requested type.</typeparam>
+        /// <typeparam name="TTo">The actual type.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="lifetimeManager">The <see cref="LifetimeManager"/> that controls the lifetime of the returned instance.</param>
+        /// <param name="injectionMembers">Injection configuration objects.</param>
+        public static IUnityContainer RegisterAsyncType<TFrom, TTo>(this IUnityContainer container, LifetimeManager lifetimeManager, params InjectionMember[] injectionMembers)
+			where TTo : TFrom
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.RegisterType<IAsyncInitializer<TFrom>, UnityAsyncInitializer<TFrom, TTo>>(lifetimeManager, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="TFrom">The requested type.</typeparam>
+        /// <typeparam name="TTo">The actual type.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="name">The name to use for registration.</param>
+        /// <param name="injectionMembers">Injection configuration objects.</param>
+        public static IUnityContainer RegisterAsyncType<TFrom, TTo>(this IUnityContainer container, string name, params InjectionMember[] injectionMembers)
+			where TTo : TFrom
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.RegisterType<IAsyncInitializer<TFrom>, UnityAsyncInitializer<TFrom, TTo>>(name, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="TFrom">The requested type.</typeparam>
+        /// <typeparam name="TTo">The actual type.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="name">The name to use for registration.</param>
+        /// <param name="lifetimeManager">The <see cref="LifetimeManager"/> that controls the lifetime of the returned instance.</param>
+        /// <param name="injectionMembers">Injection configuration objects.</param>
+        public static IUnityContainer RegisterAsyncType<TFrom, TTo>(this IUnityContainer container, string name, LifetimeManager lifetimeManager, params InjectionMember[] injectionMembers)
+			where TTo : TFrom
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+            return container.RegisterType<IAsyncInitializer<TFrom>, UnityAsyncInitializer<TFrom, TTo>>(name, lifetimeManager, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="TFrom">The requested type.</typeparam>
+        /// <typeparam name="TTo">The actual type.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="args">Initialization arguments.</param>
+        public static IUnityContainer RegisterAsyncType<TFrom, TTo>(this IUnityContainer container, AsyncInitArgs args)
+			where TTo : TFrom
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+			var injectionMembers = CreateInjectionMembers(args);
+            return container.RegisterType<IAsyncInitializer<TFrom>, UnityAsyncInitializer<TFrom, TTo>>(injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="TFrom">The requested type.</typeparam>
+        /// <typeparam name="TTo">The actual type.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="lifetimeManager">The <see cref="LifetimeManager"/> that controls the lifetime of the returned instance.</param>
+        /// <param name="args">Initialization arguments.</param>
+        public static IUnityContainer RegisterAsyncType<TFrom, TTo>(this IUnityContainer container, LifetimeManager lifetimeManager, AsyncInitArgs args)
+			where TTo : TFrom
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+			var injectionMembers = CreateInjectionMembers(args);
+            return container.RegisterType<IAsyncInitializer<TFrom>, UnityAsyncInitializer<TFrom, TTo>>(lifetimeManager, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="TFrom">The requested type.</typeparam>
+        /// <typeparam name="TTo">The actual type.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="name">The name to use for registration.</param>
+        /// <param name="args">Initialization arguments.</param>
+        public static IUnityContainer RegisterAsyncType<TFrom, TTo>(this IUnityContainer container, string name, AsyncInitArgs args)
+			where TTo : TFrom
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+			var injectionMembers = CreateInjectionMembers(args);
+            return container.RegisterType<IAsyncInitializer<TFrom>, UnityAsyncInitializer<TFrom, TTo>>(name, injectionMembers);
+        }
+
+        /// <summary>
+        /// Registers the specified asynchronously initialized type with the container.
+        /// </summary>
+        /// <typeparam name="TFrom">The requested type.</typeparam>
+        /// <typeparam name="TTo">The actual type.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="name">The name to use for registration.</param>
+        /// <param name="lifetimeManager">The <see cref="LifetimeManager"/> that controls the lifetime of the returned instance.</param>
+        /// <param name="args">Initialization arguments.</param>
+        public static IUnityContainer RegisterAsyncType<TFrom, TTo>(this IUnityContainer container, string name, LifetimeManager lifetimeManager, AsyncInitArgs args)
+			where TTo : TFrom
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+			var injectionMembers = CreateInjectionMembers(args);
+            return container.RegisterType<IAsyncInitializer<TFrom>, UnityAsyncInitializer<TFrom, TTo>>(name, lifetimeManager, injectionMembers);
+        }
+
+		private static InjectionConstructor[] CreateInjectionMembers(AsyncInitArgs args)
+		{
+            if (args == null)
+                throw new ArgumentNullException("args");
+			return new[] { new InjectionConstructor(args) };
+		}
+	}
+}
