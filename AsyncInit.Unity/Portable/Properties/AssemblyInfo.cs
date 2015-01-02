d5e215a1e9d328a8b34d23f3f063de9d6079a63b
Dmitry Shechtman <damnian@gmail.com>
2015-01-02 19:19:13 +0200

    Added AsyncInit.Unity

---
+++ AsyncInit.Unity/Portable/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ditto.AsyncInit.Unity")]
+[assembly: AssemblyDescription("AsyncInit Unity Dependency Injection")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ComVisible(false)]
---
+++ AsyncInit.Unity/Portable/UnityContainerStrategy.cs
@@ -0,0 +1,55 @@
+﻿using Ditto.AsyncInit.Services;
+using Microsoft.Practices.Unity;
+using System;
+
+namespace Ditto.AsyncInit.Unity
+{
+    /// <summary>
+    /// Unity dependency injection container strategy.
+    /// </summary>
+    public class UnityContainerStrategy : IContainerStrategy
+    {
+        private readonly IUnityContainer _container;
+        private readonly DependencyOverride _override;
+
+        /// <summary>
+        /// Creates a new strategy.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        public UnityContainerStrategy(IUnityContainer container)
+        {
+            this._container = container;
+            this._override = new DependencyOverride<UnityContainerStrategy>(this);
+        }
+
+        /// <summary>
+        /// Builds up an instance of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of object to build up.</typeparam>
+        /// <param name="value">The object to build up.</param>
+        public void BuildUp<T>(T value)
+        {
+            _container.BuildUp(value, _override);
+        }
+
+        /// <summary>
+        /// Attempts to resolve an instance of the specified type.
+        /// </summary>
+        /// <param name="type">The type of object to resolve.</param>
+        /// <param name="value">Resolved value (or <value>null</value> if unsuccessful).</param>
+        /// <returns><value>true</value> if successful or <value>false</value> otherwise.</returns>
+        public bool TryResolve(Type type, out object value)
+        {
+            try
+            {
+                value = _container.Resolve(type, _override);
+                return true;
+            }
+            catch (ResolutionFailedException)
+            {
+                value = null;
+                return false;
+            }
+        }
+    }
+}
