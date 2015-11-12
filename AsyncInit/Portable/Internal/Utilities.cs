171e7b32d151e9af3ca86920125189366fa735bc
Dmitry Shechtman <damnian@gmail.com>
2015-11-12 19:38:51 +0200

    Fix MissingMemberException message in Portable.Net45

--- AsyncInit/Portable/Internal/Utilities.cs
+++ AsyncInit/Portable/Internal/Utilities.cs
@@ -19,7 +19,7 @@ namespace Ditto.AsyncInit.Internal
             var ctor = typeof(T).GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
                 .SingleOrDefault(c => c.GetParameters().Length == 0);
             if (ctor == null)
-                throw new MissingMemberException("No parameterless constructor defined for this object.");
+                throw new MissingMemberException("No parameterless constructor is defined for this type.");
             return (T)ctor.Invoke(null);
         }
     }
