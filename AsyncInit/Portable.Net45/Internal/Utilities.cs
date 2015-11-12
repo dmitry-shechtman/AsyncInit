14eb3d2a3dfff8ac7223a2178fb878e6c953c180
Dmitry Shechtman <damnian@gmail.com>
2015-11-12 18:48:25 +0200

    Fix static constructor bug

--- AsyncInit/Portable.Net45/Internal/Utilities.cs
+++ AsyncInit/Portable.Net45/Internal/Utilities.cs
@@ -17,7 +17,7 @@ namespace Ditto.AsyncInit.Internal
         public static T CreateInstance<T>()
         {
             var typeInfo = typeof(T).GetTypeInfo();
-            var ctor = typeInfo.DeclaredConstructors.SingleOrDefault(c => c.GetParameters().Length == 0);
+            var ctor = typeInfo.DeclaredConstructors.SingleOrDefault(c => !c.IsStatic && c.GetParameters().Length == 0);
             if (ctor == null)
                 throw new MissingMemberException("No parameterless constructor defined for this object.");
             return (T)ctor.Invoke(null);
