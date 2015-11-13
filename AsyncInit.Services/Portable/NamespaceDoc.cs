ae375fb8e992647a42638905ec38b01e911b2a57
Dmitry Shechtman <damnian@gmail.com>
2015-11-14 00:58:25 +0200

    Add NamespaceDoc

---
+++ AsyncInit.Services/Portable/NamespaceDoc.cs
@@ -0,0 +1,10 @@
+﻿namespace Ditto.AsyncInit.Services
+{
+    /// <summary>
+    /// AsyncInit Dependency Injection Support classes and interfaces.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    sealed class NamespaceDoc
+    {
+    }
+}
