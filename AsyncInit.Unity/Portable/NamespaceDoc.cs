da7b6baf09c28ad4fa04b3d376c2d5171b4f78dc
Dmitry Shechtman <damnian@gmail.com>
2015-11-14 19:33:51 +0200

    Add NamespaceDoc

---
+++ AsyncInit.Unity/Portable/NamespaceDoc.cs
@@ -0,0 +1,10 @@
+﻿namespace Ditto.AsyncInit.Unity
+{
+    /// <summary>
+    /// AsyncInit Unity Dependency Injection classes.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    sealed class NamespaceDoc
+    {
+    }
+}
