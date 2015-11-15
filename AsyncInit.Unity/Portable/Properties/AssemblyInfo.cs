1ef498c6f4639c7a2e949b657356b535016fa2f7
Dmitry Shechtman <damnian@gmail.com>
2015-11-15 04:06:07 +0200

    Fix AssemblyVersion

--- AsyncInit.Unity/Portable/Properties/AssemblyInfo.cs
+++ AsyncInit.Unity/Portable/Properties/AssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Ditto.AsyncInit.Unity")]
 [assembly: AssemblyDescription("Unity Container Async Extensions")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.*")]
 
 [assembly: ComVisible(false)]
