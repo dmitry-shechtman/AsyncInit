b002096bd9159279b29ea1ec0cf9a0d1e001c098
Dmitry Shechtman <damnian@gmail.com>
2015-11-15 03:16:49 +0200

    Fix AssemblyVersion

--- AsyncInit/Net45/Properties/AssemblyInfo.cs
+++ AsyncInit/Net45/Properties/AssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Ditto.AsyncInit")]
 [assembly: AssemblyDescription("A generic implementation of the asynchronous factory pattern.")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.*")]
 
 [assembly: ComVisible(false)]
