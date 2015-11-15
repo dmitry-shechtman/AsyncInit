37c423b0f0e5f5651769358e581f03d78a268766
Dmitry Shechtman <damnian@gmail.com>
2015-11-15 03:35:21 +0200

    Fix AssemblyVersion

--- AsyncInit.Services/Portable/Properties/AssemblyInfo.cs
+++ AsyncInit.Services/Portable/Properties/AssemblyInfo.cs
@@ -3,5 +3,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Ditto.AsyncInit.Services")]
 [assembly: AssemblyDescription("AsyncInit Dependency Injection Support")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.*")]
