380eee220ca7d34e94d8ad135a177161c5490c2b
Dmitry Shechtman <damnian@gmail.com>
2015-11-15 16:59:08 +0200

    Separate Querying Named

--- AsyncInit.Unity/Portable/UnityIsAsyncTypeRegisteredExtensions.cs
+++ AsyncInit.Unity/Portable/UnityIsAsyncTypeRegisteredExtensions.cs
@@ -8,6 +8,7 @@ namespace Ditto.AsyncInit.Unity
     /// Adds a set of asynchronous registration check methods to <see cref="IUnityContainer"/>.
     /// </summary>
     /// <conceptualLink target="6f316ae1-44bb-4773-975b-aac2f5a50c49" />
+    /// <conceptualLink target="d38428af-c141-4f56-9a1d-8732f6f9a621" />
     public static class UnityIsAsyncTypeRegisteredExtensions
     {
         /// <summary>
@@ -33,7 +34,7 @@ namespace Ditto.AsyncInit.Unity
         /// <param name="name">The name to check.</param>
         /// <returns><c>true</c> if this type/name pair has been registered.</returns>
         /// <seealso cref="UnityContainerExtensions.IsRegistered{T}(IUnityContainer,String)" />
-        /// <conceptualLink target="6f316ae1-44bb-4773-975b-aac2f5a50c49" />
+        /// <conceptualLink target="d38428af-c141-4f56-9a1d-8732f6f9a621" />
         public static bool IsAsyncTypeRegistered<T>(this IUnityContainer container, string name)
         {
             if (container == null)
